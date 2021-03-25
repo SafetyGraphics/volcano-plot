03f39ec5573b0581b3cf1310f3213eb6f805b7c0
Xiao Ni <mooncase@gmail.com>
2021-03-25 13:25:15 -0400

    Cleaned pkg structure

---
+++ app.R
@@ -0,0 +1,3 @@
+pkgload::load_all(helpers = FALSE, attach_testthat = FALSE) # export_all = FALSE # ac removed: if false, IDEAFilter fails
+# rsconnect::writeManifest() # ac added
+run_app() # add parameters here (if any)
